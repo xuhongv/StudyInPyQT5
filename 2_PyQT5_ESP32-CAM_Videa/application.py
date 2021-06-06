8c40e76330b0acdd3f4672b68bdb7139514c31a8
xuhongv-9600kf <870189248@qq.com>
2021-06-06 13:06:22 +0800

    update open baud bugs

--- 2_PyQT5_ESP32-CAM_Videa/application.py
+++ 2_PyQT5_ESP32-CAM_Videa/application.py
@@ -6,7 +6,7 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import QByteArray
 from Ui_MainWindow import Ui_MainWindow
 from xUart.UartSerial import *
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5.QtGui import QImage, QPixmap
 from io import BytesIO
 from PIL import Image
@@ -20,9 +20,13 @@ pictrureName = 'what.JPG'
 
 
 def uart_callback_handler(obj):
-
     if obj['code'] == 1:
         print('串口异常断开')
+        if ui.bt_open.text() != '打开串口':
+            ui.cb_ports.setEnabled(True)
+            QMessageBox.critical(MainWindow, '错误信息', '串口异常断开！')
+            ui.bt_open.setText('打开串口')
+            refreshPort()
     else:
         buff = (obj['data'])
         length = obj['length']
@@ -36,18 +40,49 @@ def uart_callback_handler(obj):
             imgByteArr = BytesIO()  # 初始化一个空字节流
             roiimg.save(imgByteArr, format('PNG'))  # 把我们得图片以‘PNG’保存到空字节流
             imgByteArr = imgByteArr.getvalue()  # 无视指针，获取全部内容，类型由io流变成bytes。
-            img_name = '1.jpg'
+            img_name = '1.png'
             with open(os.path.join('', img_name), 'wb') as f:
                 f.write(imgByteArr)
             image = QPixmap(img_name)
             ui.label.setPixmap(image)
             picBuff.clear()
 
+
 def OnClickSend():
     buff = [0xF0, 0xA1]
     input_s = bytes(buff)
     mXUart.send_data(input_s)
     print("发送成功！！！")
+
+
+def OnClickOpen():
+    if len(GET_PORT_ARRAY) == 0:
+        print("获取电脑没有可用的串口！！！")
+    else:
+        # 设置通信串口，入参必须是字符串，比如 COM11
+        baud = 115200
+        port = ui.cb_ports.currentText()
+        str = ui.bt_open.text()
+        # 设置打开串口参数
+        mXUart.set_default_parity('N')
+        mXUart.set_default_stopbits(1)
+        mXUart.set_default_bytesize(8)
+
+        if str == '关闭串口':
+            if mXUart.try_off_port(port, baud):
+                print("关闭串口成功！！！")
+                ui.cb_ports.setEnabled(True)
+                ui.bt_open.setText("打开串口")
+            else:
+                QMessageBox.critical(MainWindow, '错误信息', '串口被占用或已拔开，无法打开！')
+                print('错误信息', '串口被占用或已拔开，无法打开')
+        if str == '打开串口':
+            if mXUart.try_port_open(port, baud):
+                ui.cb_ports.setEnabled(False)
+                print("打开串口成功：", port)
+                ui.bt_open.setText("关闭串口")
+            else:
+                QMessageBox.critical(MainWindow, '错误信息', '串口被占用或已拔开，无法打开！')
 
 
 # 刷新串口
@@ -80,43 +115,11 @@ if __name__ == '__main__':
     # 设置点击下拉框刷新电脑可用串口
     ui.cb_ports.popupAboutToBeShown.connect(refreshPort)
     ui.bt_get_pic.clicked.connect(OnClickSend)
+    ui.bt_open.clicked.connect(OnClickOpen)
     #  初始化mXUart
     mXUart = UartSerial()
     # 设置串口数据回调函数
     mXUart.setCallBack(uart_callback_handler)
     refreshPort()
-
-    # ui.graphicsView.setScene()
-
-    # if len(get_port_array_str) == 0:
-    #     print("获取电脑没有可用的串口！！！")
-    # else:
-    #     # 设置通信串口，入参必须是字符串，比如 COM11
-    baud = 115200
-    port = 'COM3'
-    #     str = '打开串口'
-    #
-    #     # 设置打开串口参数
-    mXUart.set_default_parity(
-        'N')  # PARITY_NONE, PARITY_EVEN, PARITY_ODD, PARITY_MARK, PARITY_SPACE = 'N', 'E', 'O', 'M', 'S'
-    mXUart.set_default_stopbits(1)  # STOPBITS_ONE, STOPBITS_ONE_POINT_FIVE, STOPBITS_TWO = (1, 1.5, 2)
-    mXUart.set_default_bytesize(8)  # FIVEBITS, SIXBITS, SEVENBITS, EIGHTBITS = (5, 6, 7, 8)
-
-    #
-    #     if str == '关闭串口':
-    #         if mXUart.try_off_port(port, baud):
-    #             print("关闭串口成功！！！")
-    #         else:
-    #             print('错误信息', '串口被占用或已拔开，无法打开')
-    #     if str == '打开串口':
-    #         if mXUart.try_port_open(port, baud):
-    #             print("打开串口成功：", port)
-    #         else:
-    #             print('错误信息', '串口被占用或已拔开，无法打开')
-
-    if mXUart.try_port_open(port, 115200):
-        print("打开串口成功：", port)
-    else:
-        print('错误信息', '串口被占用或已拔开，无法打开')
 
     sys.exit(app.exec_())
